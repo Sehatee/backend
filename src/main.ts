1f44fa38a7870964b4eebc9c40d393ecfa64a5b1
imad kasse <kasseimad81@gmail.com>
2025-05-25 09:53:40 +0100

    edit CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://www.sehatte.com']
   });
   await app.listen(process.env.PORT ?? 4000);
 }
